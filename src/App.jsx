22bd4096d671d024a01ab003c334c148fdc0eeb6
Aniketraje19 <aniketraje019@gmail.com>
2025-05-02 19:57:42 +0530

    updated header

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,29 @@ import PokemonList from "./features/pokemon/components/PokemonList";
 import { PokemonProvider } from './features/pokemon/context/PokemonContext';
 import { FavoritesContextProvider } from './features/pokemon/context/FavoritesContext';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import PokemonDetail from "./features/pokemon/components/PokemonDetail"
+import PokemonDetail from "./features/pokemon/components/PokemonDetail";
 import Favorites from './features/pokemon/components/Favorites';
 import Compare from './features/pokemon/components/Compare';
-import ErrorBoundary from "./ui/ErrorBoundary"
+import ErrorBoundary from "./ui/ErrorBoundary";
 
 function App() {
-
   return (
     <div className="min-h-screen bg-[#EEDDC9] p-4 text-[#3e3e3e]">
-      <Header />
-      <PokemonProvider>
-      <FavoritesContextProvider>
       <ErrorBoundary>
-      <Router>
-      <Routes>
-        <Route path="/" element={<PokemonList />} />
-        <Route path="/pokemon/:id" element={<PokemonDetail />} />
-        <Route path='/favorites' element={<Favorites />} />
-        <Route path="/compare" element={<Compare />} />
-      </Routes>
-    </Router>
-    </ErrorBoundary>
-    </FavoritesContextProvider>
-      </PokemonProvider>
+        <Router>
+          <PokemonProvider>
+            <FavoritesContextProvider>
+              <Header />
+              <Routes>
+                <Route path="/" element={<PokemonList />} />
+                <Route path="/pokemon/:id" element={<PokemonDetail />} />
+                <Route path='/favorites/' element={<Favorites />} />
+                <Route path="/compare/" element={<Compare />} />
+              </Routes>
+            </FavoritesContextProvider>
+          </PokemonProvider>
+        </Router>
+      </ErrorBoundary>
     </div>
   );
 }
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,18 +1,30 @@
 import { Link } from "react-router-dom";
+
 function Header() {
-    return (
-        <header className="bg-red-500 m-2 rounded-full shadow-lg">
-        <div className="text-center  text-white p-4  text-3xl font-bold">
+  return (
+    <header className="bg-red-500 m-4 rounded-full shadow-lg p-4">
+      <div className="text-center text-white text-3xl sm:text-4xl lg:text-5xl font-bold mb-4">
         Pokémon Explorer
-        </div>
-        <div>
-          
-          <button className="text-white">Compare Pokemon</button>
-         
-        </div>
-        
-      </header>
-    );
-  }
-  
-  export default Header;
+      </div>
+      <div className="flex flex-col sm:flex-row justify-center gap-4 mb-4 items-center">
+        <Link to={"/"}>
+          <button className="bg-white text-red-500 py-2 px-6 rounded-full hover:bg-red-100 transition-all duration-300">
+            Home
+          </button>
+        </Link>
+        <Link to={"/compare/"}>
+          <button className="bg-white text-red-500 py-2 px-6 rounded-full hover:bg-red-100 transition-all duration-300">
+            Compare Pokemon
+          </button>
+        </Link>
+        <Link to={"/favorites/"}>
+          <button className="bg-white text-red-500 py-2 px-6 rounded-full hover:bg-red-100 transition-all duration-300">
+            Favorites
+          </button>
+        </Link>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
